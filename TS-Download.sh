bf20d92feb2d5294098d56939217ebb2a324b023
polofos <leopoldo.fosygmail.com>
2025-01-19 11:52:08 -0600

    delete TS_URLs.txt

--- TS-Download.sh
+++ TS-Download.sh
@@ -4,11 +4,16 @@ while read line; do
 done < TS_URLS.txt
 
 for f in *.ts ; do
-    echo file \'$f\' >> list.txt;
+   echo file \'$f\' >> list.txt;
 done
 
 ffmpeg -f concat -safe 0 -i list.txt -c copy Temp.ts
 
 ffmpeg -i Temp.ts -map 0 -c copy $1
 
-rm *.ts
+if [ $? -ne 0 ]; then  
+    echo "Something went wrong :("  
+    exit 1
+else
+    rm *.ts list.txt TS_URLS.txt
+fi
